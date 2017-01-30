f9df2827911ca528d9d881ee7ef432e8fb4d1417
TaHuC <TaHuC@hotmail.com>
2017-01-30 21:44:32 +0200

    Compelte Math Power

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MathPower
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			double number = double.Parse(Console.ReadLine());
+			int power = int.Parse(Console.ReadLine());
+
+			Console.WriteLine(RaiseToPower(number, power));
+		}
+
+		public static double RaiseToPower(double number, int power)
+		{
+			double result = 0d;
+
+			result = Math.Pow(number, power);
+
+			return result;
+		}
+	}
+}
